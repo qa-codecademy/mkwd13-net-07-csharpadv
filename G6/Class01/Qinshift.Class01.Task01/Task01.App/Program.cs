64d72eaf8966fc161e3ed4ae5060f71ba59667a1
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-04-24 18:32:41 +0200

    Task 01 init

---
+++ G6/Class01/Qinshift.Class01.Task01/Task01.App/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("================ TASK 01 ================");
+/*
+    ## 1. Create a console application that detects provided names in a provided text
+    * The application should ask for names to be entered until the user enters 'x'.
+    * After that, the application should ask for a text.
+    * When that is done, the application should show how many times each name was included in the text (ignoring case). 
+*/
+
+List<string> names = new List<string> { "John", "AnnA", "mark", "LiSa" };
+
+string text = @"
+    Once upon a time in a small village, there lived a man named John. John was known for his kindness and generosity.
+    His neighbor, Anna, often came by to borrow sugar or just chat. One day, John and Anna decided to organize a small community event.
+    They invited their friends Mark and Lisa to help with the arrangements. Lisa baked cookies while Mark handled the music.
+    During the event, everyone praised John and Anna for their efforts. Later that evening, John wrote a thank-you note to Lisa and Mark.
+    The next day, Mark visited John again, and they talked about how successful the event was.
+    Even Anna stopped by with a basket of fruit as a thank-you gift. Everyone agreed that it was a day to remember, thanks to John, Anna, Mark, and Lisa.
+";
+
---
+++ G6/Class01/Qinshift.Class01.Task01/Task01.Logic/Models/NameCountResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Task01.Logic.Models
+{
+    public class NameCountResult
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+}
