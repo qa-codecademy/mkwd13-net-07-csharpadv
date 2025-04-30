2baf04b6a56e4a09c4cb0e9e696c63ac331946d9
Martin <panovski.martin93@gmail.com>
2025-04-30 18:33:54 +0200

    Class 04 demo code part 1 added

---
+++ G2/Class04/Qinshift.Class04/Qinshift.Generics/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Qinshift.Generics.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ G2/Class04/Qinshift.Class04/Qinshift.Generics/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Qinshift.Generics.Entities;
+using Qinshift.Generics.Helpers;
+
+NotGenericHelper notGenericHelper = new();
+
+List<string> names = new List<string>() { "Martin", "Dare", "Slave", "Ana" };
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+notGenericHelper.GetInfoForStrings(names);
+notGenericHelper.GoThroughStrings(names);
+
+notGenericHelper.GetInfoForInts(numbers);
+notGenericHelper.GoTroughInts(numbers);
+
+
+
+Console.WriteLine("================ Generic Helper ================");
+
+// If the members of the Generic class are not static then we need to 
+// create an instance of that class like:
+
+//GenericListHelper<decimal> genericListHelper = new();
+
+List<bool> booleans = new List<bool> { true, false };
+
+
+GenericListHelper<string>.GetInfoForItems(names);
+GenericListHelper<int>.GetInfoForItems(numbers);
+GenericListHelper<bool>.GetInfoForItems(booleans);
+
+
+GenericListHelper<string>.GoThroughItems(names);
+GenericListHelper<int>.GoThroughItems(numbers); 
+GenericListHelper<bool>.GoThroughItems(booleans);
+
+
+List<Product> products = new List<Product>()
+{
+    new Product() {Name = "Prod 1", Description = "Prod desc 1"},
+    new Product() {Name = "Prod 2", Description = "Prod desc 2"},
+};
+GenericListHelper<Product>.GoThroughItems(products);
