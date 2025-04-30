cb4797a581bfcd538c1297254c68e875a2a9eb03
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-04-30 20:51:53 +0200

    Class 03 finished

--- G3/Class03/Qinshift.Class03/StaticClasses/OrdersStaticDB.cs
+++ G3/Class03/Qinshift.Class03/StaticClasses/OrdersStaticDB.cs
@@ -6,15 +6,19 @@ namespace StaticClasses
 {
     public static class OrdersStaticDB
     {
-        private static int orderIdCounter;
+        private static int orderIdCounter = 5;
         public static List<User> Users = new List<User>();
         public static List<Order> Orders = new List<Order>();
 
+        // Won't work !!!
         //public OrdersStaticDB()   
         //{
-            
+
         //}
 
+        // NOTE: Static classes can have only static constructor
+        // It will only execute once, the first time this class is used, when the app is started
+        // Static constructor does not have access modifier
         static OrdersStaticDB()
         {
             ConsoleHelper.WriteInColor("Hello from OrdersStaticDB static constructor !", ConsoleColor.Yellow);
@@ -38,6 +42,24 @@ namespace StaticClasses
             Users[0].Orders.Add(Orders[2]);
             Users[1].Orders.Add(Orders[3]);
             Users[1].Orders.Add(Orders[4]);
+        }
+
+        public static void InsertOrder(int userId, Order order)
+        {
+            order.Id = ++orderIdCounter;
+            Orders.Add(order);
+            Users.First(u => u.Id == userId).Orders.Add(order);
+
+            ConsoleHelper.WriteInColor($"Order successfully created! Order Id: {order.Id}", ConsoleColor.Green);
+            ConsoleHelper.WriteInColor($"Total number of orders in the system: {Orders.Count}", ConsoleColor.Blue);
+        }
+
+        public static void ListUsers()
+        {
+            for (int i = 0; i < Users.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Users[i].Username} has {Users[i].Orders.Count} orders.");
+            }
         }
     }
 }
