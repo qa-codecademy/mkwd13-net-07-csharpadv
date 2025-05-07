f89b9bd524899e2536feca672b015fe2374668a9
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-07 19:39:40 +0200

    Class 05 init

---
+++ G3/Class04/Qinshift.Class04/ExtensionMethods/Helpers/ListHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtensionMethods.Helpers
+{
+    public static class ListHelper
+    {
+        public static void PrintStrings(this List<string> strings)
+        {
+            foreach (string item in strings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Print<T>(this List<T> list)
+        {
+            foreach (T item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- G3/Class04/Qinshift.Class04/ExtensionMethods/Program.cs
+++ G3/Class04/Qinshift.Class04/ExtensionMethods/Program.cs
@@ -1,18 +1,22 @@
 ﻿using ExtensionMethods.Helpers;
+using ExtensionMethods.Models;
 
 Console.ForegroundColor = ConsoleColor.DarkCyan;
 Console.WriteLine("\n=================== EXTENSION METHODS ===================\n");
 Console.ResetColor();
 
-
 #region String Extensions examples
+Console.WriteLine("\n============== String Extensions ==============\n");
+
 string text = "C# Advanced is an awesome subject with great demos and activities!";
 Console.WriteLine(text.ToUpper());
 Console.WriteLine(text.ToLower());
 Console.WriteLine(string.Join("JOIN", text.Split(' ')));
 Console.WriteLine(text.Replace("awesome", "tapa"));
 
-//  text.Shorten
+// Since we don't have a built-in method for shortening strings, we can create one and extend the strings functionalities with this extension method
+
+// NOTE: Don't need to access it as a regular static class, even if it is techincally possible
 // string shortenString = StringExtensions.Shorten(text, 5);
 // Console.WriteLine(shortenString);
 
@@ -25,4 +29,33 @@ Console.WriteLine(quotedString);
 Console.WriteLine("Bla Bla bla".QuoteString());
 
 Console.WriteLine(32.ToNumberString());
+#endregion
+
+
+#region List Extensions example
+Console.WriteLine("\n============== List Extensions ==============\n");
+
+List<string> strings = new() { "str1", "str2", "str3" };
+//ListHelper.PrintStrings(strings);
+
+strings.PrintStrings();
+
+List<int> integers = new() { 3214, 34, 54, 6534543 };
+integers.Print();
+strings.Print();
+#endregion
+
+
+#region Product Extensions
+Console.WriteLine("\n============== Product Extensions ==============\n");
+
+List<Product> products = new List<Product>()
+{
+    new() { Id = 1, Title = "Product 1", Description = "Description 1"},
+    new() { Id = 2, Title = "Product 2", Description = "Description 2"},
+    new() { Id = 3, Title = "Product 3", Description = "Description 3"},
+};
+
+products.Print();
+products.PrintInfo();
 #endregion
