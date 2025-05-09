87165a3d6e3bbd0b5f98e71c03bffb56c7682b02
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-09 19:47:37 +0200

    Class 06

---
+++ G3/Class06/Qinshift.Class06/ExtensionsRecap/ProductExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtensionsRecap
+{
+    public static class ProductExtensions
+    {
+        public static double GetTotalPrice(this Product product, int quantity, bool isOnPromotion)
+        {
+            if (isOnPromotion)
+            {
+                return product.Price * quantity - 100;        
+            }
+            return product.Price * quantity;        
+        }
+    }
+}
---
+++ G3/Class06/Qinshift.Class06/LinqMethods/Program.cs
@@ -0,0 +1,65 @@
+﻿using LinqMethods.Helpers;
+
+ConsoleHelper.PrintInColor("\n==================== LINQ ====================\n", ConsoleColor.DarkCyan);
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Where ==================\n", ConsoleColor.Red);
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Select ==================\n", ConsoleColor.Yellow);
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== First/Single/Last (OrDefault) ==================\n", ConsoleColor.Green);
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Select Many ==================\n", ConsoleColor.Red);
+
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Distinct/DistinctBy ==================\n", ConsoleColor.Yellow);
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Any/All ==================\n", ConsoleColor.Green);
+
+
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== Except ==================\n", ConsoleColor.Red);
+
+
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== OrderBy/ThenBy (Descending) ==================\n", ConsoleColor.Yellow);
+
+
+
+
+
+
+ConsoleHelper.PrintInColor("\n================== GroupBy ==================\n", ConsoleColor.Green);
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
