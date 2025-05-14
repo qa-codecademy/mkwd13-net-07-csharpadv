206ddaf0c9bcbf17c3a106171bda3ce52bfa64d0
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-14 20:52:08 +0200

    Workshop part 2

---
+++ G3/Class08/TaxiManager9000/TaxiManager9000.Domain/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace TaxiManager9000.Domain.Models
+{
+    public class Car : BaseEntity
+    {
+        public string Model { get; set; } = string.Empty;
+        public string LicensePlate { get; set; } = string.Empty;
+        public DateTime LicensePlateExpieryDate { get; set; }
+
+        public List<Driver> AsignedDrivers { get; set; } = new();
+
+        public override string GetInfo()
+        {
+            string drivers = string.Empty;
+            foreach (Driver driver in AsignedDrivers)
+            {
+                drivers += $"{driver.Id}.) {driver.FirstName} {driver.LastName}\n";
+            }
+            return $"Car {Model} with plates {LicensePlate} that expire on {LicensePlateExpieryDate.ToShortDateString()} is driven by : \n{drivers}";
+        }
+    }
+}
---
+++ G3/Class08/TaxiManager9000/TaxiManager9000.Domain/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using TaxiManager9000.Domain.Enums;
+
+namespace TaxiManager9000.Domain.Models
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Role Role { get; set; }
+
+        public User(string username, string password, Role role)
+        {
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+
+        public User()
+        {
+
+        }
+
+        public override string GetInfo()
+        {
+            return $"User with username: [{Username}] and role: [{Role}]";
+        }
+    }
+}
