c34b8d53b7537d247e6589347cbd4694efd2e0c5
Martin <panovski.martin93@gmail.com>
2025-05-15 18:30:01 +0200

    Class 07 demo code part 1 added

---
+++ G2/Class07/Qinshift.Class07/Qinshift.Delegates/Program.cs
@@ -0,0 +1,56 @@
+﻿
+#region Delegates - Declare and Instantiating a delegate
+
+
+
+
+#endregion
+
+
+#region Passing a method to a delegate parameter
+
+
+
+
+#endregion
+
+
+
+class Program
+{
+    static void SayHello(string person)
+    {
+        Console.WriteLine($"Hello there dear, {person}");
+    }
+
+    static void SayGoodBye(string person)
+    {
+        Console.WriteLine($"Goodbye {person}, see you next time!");
+    }
+
+    static void PrintNumber(int number)
+    {
+        Console.WriteLine(number);
+    }
+
+    void SayWhatever(string whatever, SayDelegate sayDel)
+    {
+
+    }
+
+    public delegate void SayDelegate(string person);
+
+    static void Main(string[] args)
+    {
+        SayDelegate sayHelloDel = new SayDelegate(SayHello);
+        SayDelegate sayGoodByeDel = new SayDelegate(SayGoodBye);
+        SayDelegate sayWoowDel = new SayDelegate(x => Console.WriteLine($"Woooow {x}"));
+
+
+        //SayDelegate printNumDel = new SayDelegate(PrintNumber);   ---> this will not work, because the signature of the PrintNumber method doesn't fit with the signature of the SayDelegate delegate
+
+        sayHelloDel("Bob");
+        sayGoodByeDel("Martin");
+        sayWoowDel("Slave");
+    }
+}
