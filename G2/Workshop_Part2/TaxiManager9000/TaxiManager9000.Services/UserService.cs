7cf388f46addf6a183d4e91a08b2c2388885d676
Martin <panovski.martin93@gmail.com>
2025-05-20 20:43:11 +0200

    Class 08 demo code part 2 added

---
+++ G2/Workshop_Part2/TaxiManager9000/TaxiManager9000.Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using TaxiManager9000.Domain.Models;
+using TaxiManager9000.Services.Interfaces;
+
+namespace TaxiManager9000.Services
+{
+    public class UserService : ServiceBase<User>, IUserService
+    {
+        public User CurrentUser { get; set; }
+
+        public bool ChangePassword(string oldPassword, string newPassword)
+        {
+            User user = _dbInstance.GetById(CurrentUser.Id);
+            if (user.Password != oldPassword)
+                return false;
+            user.Password = newPassword;
+            _dbInstance.Update(user);
+            return true;
+        }
+
+        public void LogIn(string username, string password)
+        {
+            CurrentUser = _dbInstance
+                .GetAll()
+                .SingleOrDefault(x => x.Username.ToLower() == username.ToLower() && x.Password == password);
+
+            if (CurrentUser is null)
+                throw new Exception("Login Failed! Please try again");
+        }
+    }
+}
