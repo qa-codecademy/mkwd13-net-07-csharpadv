5091b23a6e49418ce6fee62c5e5f01018cc2bc35
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-21 20:02:45 +0200

    Class 10 Disposable

---
+++ G3/Class10/Qinshift.Class10/Qinshift.Disposable/Program.cs
@@ -0,0 +1,180 @@
+﻿using Qinshift.Disposable.OurStreamClasses;
+using Qinshift.Helpers;
+
+ExtendedConsole.PrintInColor("\n================== DISPOSING ==================\n\n", ConsoleColor.Cyan);
+// => process of releasing unmanaged resources such as file handles, database connections, and network connections
+// => it's important to dispose of these resources properly to avoid memory leaks and ensure the efficient use of resources
+
+const string AppPath = @"..\..\..\Text";
+const string FileName = @"text.txt";
+const string FilePath = $@"{AppPath}\{FileName}";
+
+if (!Directory.Exists(AppPath))
+{
+    Directory.CreateDirectory(AppPath);
+}
+
+if (!File.Exists(FilePath))
+{
+    File.Create(FilePath).Close();
+}
+
+
+#region Manual Dispose Methods
+
+void AppendTextInFile(string text, string path)
+{
+    StreamWriter sw = new StreamWriter(path, true);
+    sw.WriteLine(text);
+    sw.Dispose();
+}
+
+void ReadTextFromFile(string path)
+{
+    StreamReader sr = new StreamReader(path);
+    Console.WriteLine(sr.ReadToEnd());
+    sr.Dispose();
+}
+
+void ManualDisposeExample()
+{
+    try
+    {
+        ExtendedConsole.PrintInColor("Enter text pt1:");
+        string text1 = Console.ReadLine();
+        AppendTextInFile(text1, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt2:");
+        string text2 = Console.ReadLine();
+        AppendTextInFile(text2, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt3:");
+        string text3 = Console.ReadLine();
+        AppendTextInFile(text3, FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR WRITING IN FILE: " + ex.Message);
+    }
+
+    try
+    {
+        ExtendedConsole.PrintInColor("--------- Read ---------\n");
+        ReadTextFromFile(FilePath);
+        ReadTextFromFile(FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR READING FILE: " + ex.Message);
+    }
+}
+#endregion
+
+
+#region Dispose with Using Block
+void AppendTextInFileSafe(string text, string path)
+{
+    using (StreamWriter sw = new StreamWriter(path, true))
+    {
+        sw.WriteLine(text);
+    }
+}
+
+void ReadTextFromFileSafe(string path)
+{
+    using StreamReader sr = new StreamReader(path);
+    Console.WriteLine(sr.ReadToEnd());
+}
+
+void UsingBlockDisposeExample()
+{
+    try
+    {
+        ExtendedConsole.PrintInColor("Enter text pt1:");
+        string text1 = Console.ReadLine();
+        AppendTextInFileSafe(text1, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt2:");
+        string text2 = Console.ReadLine();
+        AppendTextInFileSafe(text2, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt3:");
+        string text3 = Console.ReadLine();
+        AppendTextInFileSafe(text3, FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR WRITING IN FILE: " + ex.Message);
+    }
+
+    try
+    {
+        ExtendedConsole.PrintInColor("--------- Read ---------\n");
+        ReadTextFromFileSafe(FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR READING FILE: " + ex.Message);
+    }
+}
+#endregion
+
+
+#region Dispose with our own disposable class
+void AppendTextInFileOurImplementation(string text, string path)
+{
+    using (OurWriter ourWriter = new OurWriter(path))
+    {
+        ourWriter.Write(text);
+    }
+}
+
+void ReadTextFromFileOurImplementation(string path)
+{
+    using (OurReader ourReader = new OurReader(path))
+    {
+        Console.WriteLine(ourReader.Read()); 
+    }
+}
+
+
+void OurDisposeExample()
+{
+    try
+    {
+        ExtendedConsole.PrintInColor("Enter text pt1:");
+        string text1 = Console.ReadLine();
+        AppendTextInFileOurImplementation(text1, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt2:");
+        string text2 = Console.ReadLine();
+        AppendTextInFileOurImplementation(text2, FilePath);
+
+        ExtendedConsole.PrintInColor("Enter text pt3:");
+        string text3 = Console.ReadLine();
+        AppendTextInFileOurImplementation(text3, FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR WRITING IN FILE: " + ex.Message);
+    }
+
+    try
+    {
+        ExtendedConsole.PrintInColor("--------- Read ---------\n");
+        ReadTextFromFileOurImplementation(FilePath);
+    }
+    catch (Exception ex)
+    {
+        ExtendedConsole.PrintError("ERROR READING FILE: " + ex.Message);
+    }
+}
+#endregion
+
+
+//ManualDisposeExample();
+//UsingBlockDisposeExample();
+OurDisposeExample();
+
+
+Console.ReadLine();
--- G3/Class10/Qinshift.Class10/Qinshift.MemoryAllocation/Program.cs
+++ G3/Class10/Qinshift.Class10/Qinshift.MemoryAllocation/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Qinshift.Helpers;
+using Qinshift.MemoryAllocation;
+
+
+#region Value & Reference types
+ExtendedConsole.PrintInColor("\n=============== STACK & HEAP ===============\n\n", ConsoleColor.DarkCyan);
+
+// STACK
+// => used for static memory allocation
+// => stores value types and reference to the reference type
+
+// HEAP
+// => used for dynamic memory allocation
+// => stores the actual data of the reference types
+// => is managed by the garbage collector (GC), which automatically frees up memory that is no longer being used
+
+ExtendedConsole.PrintInColor("\n=============== Value type & Reference type ===============\n", ConsoleColor.Cyan);
+// VALUE TYPES => both *reference* (variable) and *value* are stored on the STACK
+// bool, byte, char, int, decimal, double, enum, float, long, short, struct...
+
+// REFERENCE TYPES => *reference* is stored on the STACK, the actual *value* on the HEAP
+// classes (objects), interface, delegate, *string*...
+
+// *STRINGS* => special type, are considered reference type
+
+Console.WriteLine("======= Value types =======\n");
+int num1 = 10;
+int num2 = num1; // it assignes only the value to the new variable num2
+num2 = 50; // the change won't affect num1 
+Console.WriteLine(num1); // 10
+Console.WriteLine(num2); // 50
+
+
+Console.WriteLine("\n======= Reference types =======\n");
+
+// ===> Example: List<int>
+List<int> ints1 = new() { 1, 2, 3 };
+List<int> ints2 = ints1; // passing reference to the ints1 list
+ints2[1] = 100; // will affect the original ints1 list
+
+ints1.ForEach(i => Console.WriteLine(i)); // 1, 100, 3
+Console.WriteLine("=======");
+ints2.ForEach(i => Console.WriteLine(i)); // 1, 100, 3
+
+
+// ===> Example: User
+User john = new User
+{
+    FirstName = "John",
+    LastName = "Doe",
+    Age = 29
+};
+
+User john2 = john; // passing reference to the john object
+john2.Age = 90; // will affect john object as well
+john.LastName = "BLAAA"; // will affect john2 object as well
+
+john.PrintInto(); // John BLAAA (90)
+john2.PrintInto(); // John BLAAA (90)
+
+
+Console.WriteLine("\n======= Strings =======\n");
+
+// ===> Example: *String*
+string stringOne = "String One";
+string stringTwo = stringOne;
+stringTwo = "String Two";
+// ***NOTE*** Even though strings are REFERENCE type and the values are stored on the heap, they behave like VALUE types in many ways
+
+Console.WriteLine(stringOne); // String One
+Console.WriteLine(stringTwo); // String Two
+
+#endregion
+
+
+#region Objects Lifecycle
+
+ExtendedConsole.PrintInColor("\n\n=============== OBJECTS LIFECYCLE ===============\n", ConsoleColor.Yellow);
+
+static void TestObject()
+{
+    User bob = new User("Bob", "Bobsky", 33);
+    // ===> when using *named parameters* the order doesn't metter
+    User john = new User(age: 23, firstName: "John", lastName: "Doe");
+    Console.WriteLine("Logic with bob object...");
+    bob.PrintInto();
+    Console.WriteLine("More logic...");
+    john.PrintInto();
+    Console.WriteLine("Okay, we don't need the objects anymore. Throw them in the trash...");
+}
+
+TestObject();
+// GC.Collect(); // if you don't want to wait for the Garbage Collector to automatically collect the disposed objects ... (used for DEMO purposes)
+
+#endregion
+
+
+Console.ReadLine();
