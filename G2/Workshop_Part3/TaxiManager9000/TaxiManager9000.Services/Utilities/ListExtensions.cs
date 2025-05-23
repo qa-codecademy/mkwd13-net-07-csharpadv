26ad31bf31a04ad1d0d1fea7870ec84df69d8a76
Martin <panovski.martin93@gmail.com>
2025-05-23 18:49:20 +0200

    Class 10 demo code part 2 added

---
+++ G2/Workshop_Part3/TaxiManager9000/TaxiManager9000.Services/Utilities/ListExtensions.cs
@@ -0,0 +1,48 @@
+﻿using TaxiManager9000.Domain.Enums;
+using TaxiManager9000.Domain.Models;
+
+namespace TaxiManager9000.Services.Utilities
+{
+    public static class ListExtensions
+    {
+        private static readonly Dictionary<ExpiryStatus, ConsoleColor> StatusColorMapper = new()
+        {
+            {ExpiryStatus.Valid, ConsoleColor.Green },
+            {ExpiryStatus.Warning, ConsoleColor.Yellow },
+            {ExpiryStatus.Expired, ConsoleColor.Red }
+        };
+
+        public static void Print<T>(this List<T> list) where T : BaseEntity
+        {
+            if (list.Count == 0)
+            {
+                ExtendedConsole.NoItemsMessage<T>();
+            }
+            else
+            {
+                foreach (T item in list)
+                {
+                    Console.WriteLine(item.Print());
+                }
+            }
+
+        }
+
+        public static void PrintStatus(this List<Car> list)
+        {
+            if(list.Count == 0)
+            {
+                ExtendedConsole.NoItemsMessage<Car>();
+            }
+            else 
+            {
+                foreach (var car in list)
+                {
+                    ExpiryStatus status = car.IsLicensePlateExpired();
+                    ExtendedConsole.Write($"[{status}]) ", StatusColorMapper[status]);
+                    Console.WriteLine($"Car Id: {car.Id} - Plate: {car.LicensePlate} with expiry date: {car.LicensePlateExpiryDate}");
+                }
+            }
+        }
+    }
+}
--- G2/Workshop_Part3/TaxiManager9000/TaxiManager9000.Services/Utilities/StringValidator.cs
+++ G2/Workshop_Part3/TaxiManager9000/TaxiManager9000.Services/Utilities/StringValidator.cs
@@ -2,7 +2,32 @@
 {
     public static class StringValidator
     {
+        public static int ValidateNumber(string number, int maxValue)
+        {
+            bool isNumber = int.TryParse(number, out int num);
+            if (!isNumber || num <= 0 || num > maxValue) return -1;
+            return num;
+        }
+
+        public static bool ValidateUsername(string username)
+        {
+            if (username.Length < 6) return false;
+            return true;
+        }
 
 
+        public static bool ValidatePassword(string password)
+        {
+            if(password.Length < 6) return false;
+
+            bool isNum = false;
+            foreach (char item in password) 
+            {
+                isNum = int.TryParse(item.ToString(), out int num);
+                if (isNum)
+                    break;
+            }
+            return isNum;
+        }
     }
 }
