57f5c3e522faae2cd727d3465e526c0ad26488c6
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-23 18:54:01 +0200

    Serialization

---
+++ G3/Class10/Qinshift.Class10/Qinshift.SerializeDeserialize/Entities/ReaderWriter.cs
@@ -0,0 +1,25 @@
+﻿namespace Qinshift.SerializeDeserialize.Entities
+{
+    public class ReaderWriter
+    {
+        public string ReadFile(string path)
+        {
+            string result = string.Empty;
+
+            using (StreamReader sr = new(path, true))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        public void WriteInFile(string path, string data)
+        {
+            using (StreamWriter sw = new(path))
+            {
+                sw.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ G3/Class10/Qinshift.Class10/Qinshift.SerializeDeserialize/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Qinshift.SerializeDeserialize.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string directoryPath = @"../../../OutData";
+string fileName = "myFirstJson.json";
+string filePath = Path.Combine(directoryPath, fileName);
+OurJsonSerializer Serializer = new OurJsonSerializer();
+ReaderWriter ReaderWriter = new ReaderWriter();
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+Student bob = new Student
+{
+    FirstName = "Bob",
+    LastName = "Bobsky",
+    Age = 23,
+    IsPartTime = false
+};
+
+
+#region Manual Serialization/Deserialization
+
+string bobJsonString = Serializer.SerializeStudent(bob);
+Console.WriteLine(bob);
+Console.WriteLine(bobJsonString);
+
+ReaderWriter.WriteInFile(filePath, bobJsonString);
+
+string jsonStudent = ReaderWriter.ReadFile(filePath);
+Console.WriteLine(jsonStudent);
+
+Student deserializedStudent = Serializer.DeserializeStudent(jsonStudent);
+
+#endregion
+
+
+#region Newtonsoft JSON serialize / deserialize
+
+Console.WriteLine("============ Using Newtonsoft.json ============");
+
+string bobJsonNewtonsoft = JsonConvert.SerializeObject(bob);
+Console.WriteLine(bobJsonNewtonsoft);
+
+//object bobDeserializedNewtonSoft = JsonConvert.DeserializeObject(bobJsonNewtonsoft);
+Student bobDeserializedNewtonSoft = JsonConvert.DeserializeObject<Student>(bobJsonNewtonsoft);
+
+#endregion
+
+
+Console.ReadLine();
