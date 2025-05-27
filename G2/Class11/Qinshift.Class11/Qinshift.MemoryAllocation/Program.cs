f4ad7b566b8b9775fa8d8fe68cb70f1d8f877d75
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-27 19:49:54 +0200

    Class 11 Disposing

---
+++ G2/Class11/Qinshift.Class11/Qinshift.MemoryAllocation/Program.cs
@@ -0,0 +1,91 @@
+﻿using Qinshift.Helpers;
+using Qinshift.MemoryAllocation;
+
+Console.WriteLine("Hello, World!");
+
+ExtendedConsole.PrintInColor("\n=============== STACK & HEAP ===============\n\n", ConsoleColor.DarkCyan);
+
+// STACK
+// => part of the RAM memory used for static memory allocation
+// => stores value types and reference to the reference type
+
+// HEAP
+// => part of the RAM memory used for dynamic memory allocation
+// => stores the actual data of the reference types
+// => is managed by the garbage collector (GC), which automatically frees up memory that is no longer being used
+
+ExtendedConsole.PrintInColor("\n=============== Value type & Reference type ===============\n", ConsoleColor.Cyan);
+// VALUE TYPES => both *reference* (variable) and *value* are stored on the STACK
+// bool, byte, char, int, decimal, double, enum, float, long, short, struct...
+
+// REFERENCE TYPES => *reference* is stored on the STACK, the actual *value* on the HEAP
+// classes (objects), interface, delegate, *string*...
+
+// *STRINGS* => special type, are considered reference type
+
+Console.WriteLine("======= Value types =======\n");
+int num1 = 10; 
+int num2 = num1; // it assignes only the value to the new variable num2
+num2 = 50; // the change won't affect num1 
+
+Console.WriteLine(num1); // 10
+Console.WriteLine(num2); // 50
+
+
+Console.WriteLine("\n======= Reference types =======\n");
+
+// ===> Example: List<int>
+List<int> ints1 = new List<int> { 1, 2, 3 };
+List<int> ints2 = ints1; // passing reference to the ints1 list
+ints1[1] = 1000; // will affect the original ints1 list
+//ints2[0] = 324324;
+
+ints1.ForEach(n => Console.WriteLine(n)); // 1 1000 3
+ints2.ForEach(n => Console.WriteLine(n)); // 1 1000 3
+
+
+// ===> Example: User
+User john = new User
+{
+    FirstName = "John",
+    LastName = "Doe",
+    Age = 32
+};
+
+User john2 = john; // passing reference to the john object
+john2.Age = 80; // will affect john object as well
+
+john.PrintInfo(); 
+john2.PrintInfo();
+
+
+Console.WriteLine("\n======= Strings =======\n");
+
+// ===> Example: *String*
+string stringOne = "String One";
+string stringTwo = stringOne;
+stringTwo = "String Two";
+// ***NOTE*** Even though strings are REFERENCE type and the values are stored on the heap, they behave like VALUE types in many wayss
+
+Console.WriteLine(stringOne);
+Console.WriteLine(stringTwo);
+
+
+Console.WriteLine("\n\n=============== OBJECTS LIFECYCLE ===============\n");
+
+void TestObject()
+{
+    User bob = new User("Bob", "Bobsky", 23);
+    User johnny = new User("John", "Johnsky", 34);
+    Console.WriteLine("Logic with bob object...");
+    bob.PrintInfo();
+    Console.WriteLine("More logic ...");
+    johnny.PrintInfo();
+    Console.WriteLine("Okay, we don't need the objects anymore. Throw them in the trash...");
+
+}
+TestObject();
+// Force garbage collection
+GC.Collect(); // Important: This is only for demo. You should never do this in code — let the GC handle it automatically
+
+Console.ReadLine();
---
+++ G2/Class11/Qinshift.Class11/Qinshift.MemoryAllocation/User.cs
@@ -0,0 +1,34 @@
+﻿namespace Qinshift.MemoryAllocation
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Console.WriteLine($"User object {FirstName} created. [{DateTime.Now}]");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} - {Age}");
+        }
+
+        // Class Finalizer (Destructor) => used to perform any necessary final clean-up when a class instance is being collected by the garbage collector
+        // NOTE: This is only for DEMONSTRATION purposes, this process is automatically done. We don't need to create finalizers in any of the classes we create!
+        ~User()
+        {
+            Console.WriteLine($"User object {FirstName} destroyed. [{DateTime.Now}]");
+        }
+    }
+}
