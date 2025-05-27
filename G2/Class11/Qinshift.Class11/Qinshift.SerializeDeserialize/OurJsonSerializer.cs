e883bb492b207140dcf78eae847dceef1ef32d8e
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-05-27 20:56:04 +0200

    Class 11 Serialize

---
+++ G2/Class11/Qinshift.Class11/Qinshift.SerializeDeserialize/OurJsonSerializer.cs
@@ -0,0 +1,62 @@
+﻿namespace Qinshift.SerializeDeserialize
+{
+    /*
+        {
+            "FirstName": "Bob",
+            "LastName": "Bobsky",
+            "Age": 40,
+            "IsPartTime": false
+        }
+    */
+    public class OurJsonSerializer
+    {
+        public string SerializeStudent(Student student)
+        {
+            string json = string.Empty;
+            json += "{";
+            json += $"\"FirstName\": \"{student.FirstName}\",";
+            json += $"\"LastName\": \"{student.LastName}\",";
+            json += $"\"Age\": {student.Age},";
+            json += $"\"IsPartTime\": {student.IsPartTime.ToString().ToLower()}";
+            json += "}";
+            return json;
+        }
+
+        /*
+            {
+                "FirstName": "Bob",
+                "LastName": "Bobsky",
+                "Age": 40,
+                "IsPartTime": false
+            }
+        */
+        public Student DeserializeStudent(string studentJson)
+        {
+            // Cleaning the json 
+            string content = studentJson
+                .Substring(studentJson.IndexOf('{') + 1, studentJson.IndexOf('}') - 1)
+                .Replace("\r", "")
+                .Replace("\n", "")
+                .Replace("\"", "");
+
+            string[] properties = content.Split(',');
+
+            Dictionary<string, string> propertiesDictionary = new();
+
+            foreach (string property in properties)
+            {
+                string[] pair = property.Split(':');
+                propertiesDictionary.Add(pair[0].Trim(), pair[1].Trim());
+            }
+
+            // Creating dictionary with clean keys (properties) and values
+            Student student = new Student();
+            student.FirstName = propertiesDictionary["FirstName"];
+            student.LastName = propertiesDictionary["LastName"];
+            student.Age = int.Parse(propertiesDictionary["Age"]);
+            student.IsPartTime = bool.Parse(propertiesDictionary["IsPartTime"]);
+
+            return student;
+        }
+    }
+}
---
+++ G2/Class11/Qinshift.Class11/Qinshift.SerializeDeserialize/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Qinshift.SerializeDeserialize
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsPartTime { get; set; }
+    }
+}
