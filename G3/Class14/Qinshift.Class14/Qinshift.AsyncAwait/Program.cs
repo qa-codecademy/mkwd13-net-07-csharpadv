095634621725c6b56c13a755021348093f75ca9a
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2025-06-02 20:45:04 +0200

    Class 14

---
+++ G3/Class14/Qinshift.Class14/Qinshift.AsyncAwait/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n================= ASYNC/AWAIT =================\n\n");
+Console.ResetColor();
+
+
+void SendMessage(string message)
+{
+    Console.WriteLine("Sending message ...");
+    Thread.Sleep(5000);
+    Console.WriteLine($@"The message ""{message}"" was sent!");
+}
+
+//async void SendMessageAsync(string message)
+//{
+
+//}
+
+async Task SendMessageAsync(string message)
+{
+    Console.WriteLine("Sending message...\n");
+    await Task.Run(() => // here with 'await' the program waits for this Task to finish and then prints the message "It takes 7 sec..."
+    {
+        Thread.Sleep(7000);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($@"The message ""{message}"" was sent!");
+        Console.ResetColor();
+    });
+    Console.WriteLine("It takes 7 seconds...");
+}
+
+void ShowAd(string product)
+{
+    Console.WriteLine("\nWhile you wait let us show you an ad:");
+    Console.Write("Buy the best product in the world ");
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.Write(product);
+    Console.ResetColor();
+    Console.Write(" now and get ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("FREE");
+    Console.ResetColor();
+    Console.WriteLine(" shipping worldwide!");
+}
+
+async Task AwaitExample()
+{
+    //await SendMessageAsync("Hello there Qinshift Academy students!"); // we don't need to await this method since it doesn't affect the further flow of the program
+    SendMessageAsync("Hello there Qinshift Academy students!");
+    Console.WriteLine("\nGetting the AD item...");
+    //Task adItem = GetAdvertismentItemAsync(); // won't work
+    string adItem = await GetAdvertismentItemAsync();
+    ShowAd(adItem);
+}
+
+async Task<string> GetAdvertismentItemAsync()
+{
+    Thread.Sleep(11_000);
+    return "Keyboard";
+}
+
+
+string message = "Hello there Qinshift Academy students!";
+
+// ===> Example for synchronous execution ( It does not show the ad while we wait )
+//SendMessage(message);
+//ShowAd("Laptop");
+
+// ===> Example of asynchronous execution
+//await SendMessageAsync(message); // waits for the whole method to finish, program DOESN'T continue
+//SendMessageAsync(message); // will be executed when the task is finished, program continues
+//ShowAd("Laptop");
+
+// ===> We can assign the task to a variable
+//Task sendMessageTask = SendMessageAsync(message);
+
+
+// ===> MainThread method example
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+stopwatch.Start();
+await AwaitExample();
+stopwatch.Stop();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"\nTotal Time: {stopwatch.ElapsedMilliseconds} ms");
+Console.ResetColor();
+
+Console.ReadLine();
---
+++ G3/Class14/Qinshift.Class14/Qinshift.Tasks/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("\n================= TASKS =================\n\n");
+Console.ResetColor();
+// Tasks represent asynchronous operations
+// Unlike the threads in C#, Tasks are not opening a new execution path every time
+// They get queued to be executed asynchronously by an available thread
+// This is done by a system called the ***Thread Pool*** 
+
+Console.WriteLine("App start...\n");
+
+// ===> Single Task Example
+Task myTask = new Task(() =>
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("Running after 2000ms");
+});
+Console.WriteLine(myTask.Status);
+myTask.Start();
+Console.WriteLine(myTask.Status);
+Console.WriteLine(myTask.Status);
+
+
+// ===> Single task with return type 
+Task<int> valueTask = new Task<int>(() =>
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("We can now get the number...");
+    return 3;
+});
+valueTask.Start();
+Console.WriteLine(valueTask.Result);
+
+
+Task.Run(() =>
+{
+    Thread.Sleep(3000);
+    Console.WriteLine("This is executed after 3000ms...");
+});
+
+
+Console.WriteLine("\nApp end...\n");
+
+
+for (int i = 1; i <= 20; i++)
+{
+    int temp = i;
+    Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine($"TASK NO. {temp}");
+    });
+}
+
+
+
+
+Console.ReadLine();
